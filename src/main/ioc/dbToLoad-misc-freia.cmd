cd86a3598cc0b046998e67604305fd1130def8d1
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-03-04 13:36:43 +0100

    Pv name correction. Added check of sOn before updating Td

--- src/main/ioc/dbToLoad-misc-freia.cmd
+++ src/main/ioc/dbToLoad-misc-freia.cmd
@@ -5,7 +5,7 @@
 dbLoadRecords("rf_itlck_hb.db","P=IntlkF-cRio1:Intlk")
 #dbLoadTemplate(xxx.substitutions)
 
-dbLoadRecords "longin.template", "P=$(DMT_PREFIX)_, R=sOK, PORT=DMT_STATUS, OFFSET=0, DATA_TYPE=UINT16, PREC=0, SCAN=I/O Intr, LOPR=0, HOPR=1"
+dbLoadRecords "longin.template", "P=$(DMT_PREFIX)_, R=sOk, PORT=DMT_STATUS, OFFSET=0, DATA_TYPE=UINT16, PREC=0, SCAN=I/O Intr, LOPR=0, HOPR=1"
 dbLoadRecords "longin.template", "P=$(DMT_PREFIX)_, R=sOn, PORT=DMT_STATUS, OFFSET=1, DATA_TYPE=UINT16, PREC=0, SCAN=I/O Intr, LOPR=0, HOPR=1"
 dbLoadRecords "longin.template", "P=$(DMT_PREFIX)_, R=sErrCode1, PORT=DMT_STATUS, OFFSET=3, DATA_TYPE=UINT16, PREC=0, SCAN=I/O Intr, LOPR=0, HOPR=0xffff"
 dbLoadRecords "longin.template", "P=$(DMT_PREFIX)_, R=sErrCode2, PORT=DMT_STATUS, OFFSET=4, DATA_TYPE=UINT16, PREC=0, SCAN=I/O Intr, LOPR=0, HOPR=0xffff"
